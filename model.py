b108f0ae838fd5e185b64746b5a046e007e5453b
Abdul Aziz A.B <66479632+4bdul4ziz@users.noreply.github.com>
2022-02-02 23:44:50 +0530

    model bug fixes

--- model.py
+++ model.py
@@ -7,7 +7,7 @@ import os
 
 class Linear_QNet(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
-        super()._init()
+        super().__init__()
         self.linear1 = nn.Linear(input_size, hidden_size)
         self.linear2 = nn.Linear(hidden_size, output_size)
 
