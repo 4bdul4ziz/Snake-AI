30ef2800b2e0b3de30a48b8da11b72b7c9789f2f
Abdul Aziz A.B <66479632+4bdul4ziz@users.noreply.github.com>
2022-02-03 00:16:55 +0530

    bug fixes and final commit

--- snakeGame.py
+++ snakeGame.py
@@ -86,7 +86,7 @@ class SnakeGameAI:
         if self.is_collision() or self.frame_iteration > 100*len(self.snake):
             game_over = True
             reward = -10
-            return game_over, self.score
+            return reward, game_over, self.score
             
         # 4. place new food or just move
         if self.head == self.food:
